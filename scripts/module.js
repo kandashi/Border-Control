6af7874c77d09c1a1fda6fa16e3058a59c4105d8
kandashi <jacobmpalmer@live.co.uk>
2021-04-26 00:31:36 +0100

    first

--- scripts/module.js
+++ scripts/module.js
@@ -1,7 +1,10 @@
+import {libWrapper} from './shim.js';
+
 Hooks.once('init', async function() {
-
+    libWrapper.register('Remove-Borders', 'Token.prototype._getBorderColor', borderRemove, 'WRAPPER')
 });
 
-Hooks.once('ready', async function() {
+function borderRemove(wrapped, ...args) {
 
-});
+    return null
+}
